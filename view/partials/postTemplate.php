ad86faa3b2faa563c66d808dbf295b683bb9f0ef
nipo <saiful.islam@inspiring-travel.de>
2016-03-06 01:34:59 +0100

    added auto page refresh

---
+++ view/partials/postTemplate.php
@@ -0,0 +1,37 @@
+<div id="resultRow" class="hidden">
+    <h2 class="headline">Available Posts : </h2>
+    <div  class="list-group-item">
+        <div class="row">
+            <div class="col-md-12 pull-left title"><strong></strong></div>
+            <div class="col-md-12 shortDescription"></div>
+            <div class="col-md-2 pull-right">
+                <span><i class="glyphicon glyphicon-calendar posttime"></i></span>
+            </div>
+
+            <div class="col-md-2 pull-left">
+                <a class="viewPost" href="">
+                    <i class="glyphicon glyphicon-folder-open"></i> view details
+                </a>
+            </div>
+
+            <div class="col-md-1 pull-left editPost">
+                <a class="editPost" href="#" data-post_id="">
+                    <i class="glyphicon glyphicon-edit"></i>edit
+                </a>
+            </div>
+
+            <div class="col-md-1 pull-left deltePost">
+                <a class="deletePost" href="#" data-post_id="">
+                    <i class="glyphicon glyphicon-remove"></i>delete
+                </a>
+            </div>
+
+            <div class="col-md-2 pull-right">
+                <span><i class="glyphicon glyphicon-comment commentCount"></i> </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
